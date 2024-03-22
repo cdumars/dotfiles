6cba2d44ba8caa58ce39cf3b495e2dbc0906540c
Cooper Dumars <cooper@dumars.org>
2024-03-21 18:24:50 -0700

    add openvpn

--- system/default.nix
+++ system/default.nix
@@ -16,6 +16,7 @@ let
     ./services
     ./services/pipewire.nix
     ./services/greetd.nix
+    ./services/openvpn.nix
   ];
 
   laptop = [
---
+++ system/services/openvpn.nix
@@ -0,0 +1,5 @@
+{
+  services.openvpn.servers = {
+    los-angeles = {config = ''config /home/cooper/Documents/fastestvpn/udp/usa-losangeles-udp.ovpn '';};
+  };
+}
