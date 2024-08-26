3de2ce48cfd2a2cddf5540b0458b45d13760b2e9
Cooper Dumars <cooper@dumars.org>
2024-08-26 14:54:15 -0700

    configure nixos-cli

--- system/nix/nixos-cli.nix
+++ system/nix/nixos-cli.nix
@@ -1,3 +1,18 @@
 {
-  services.nixos-cli.enable = true;
+  services.nixos-cli = {
+    enable = true;
+    config = {
+      config_location = "/home/cooper/Documents/dotfiles";
+      no_confirm = true;
+
+      aliases = {
+        switch = ["generation" "switch"];
+        rollback = ["generation" "rollback"];
+      };
+
+      apply = {
+        use_nom = true;
+      };
+    };
+  };
 }
